d70091cae7d77b2ec8b5cc82bcd57f3e730e2e5e
ivnovyuriy <ivanovyuriy@gmail.com>
2022-12-07 09:16:17 -0500

    Maks chenges

--- infrastra/scripts/webserver.sh
+++ infrastra/scripts/webserver.sh
@@ -30,6 +30,15 @@ sudo apt install -y php7.4-curl
 sudo apt install -y php7.4-xml
 sudo apt install -y php7.4-bcmath
 
+# Installing Docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -a -G docker ubuntu
+
 # installing WordPress:latest
 
 rm /var/www/html/index.*  
