1a4ea0fabbf961997b807ed7503b19abef56b227
ivnovyuriy <ivanovyuriy@gmail.com>
2022-12-07 10:19:46 -0500

    Add EC2 external host to Jankins file

--- infrastra/scripts/jenkins.sh
+++ infrastra/scripts/jenkins.sh
@@ -36,6 +36,8 @@ sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install -y jenkins
+
+# Add jenkins user to docker group
 sudo usermod -a -G docker jenkins
 
 # Starting Jenkins
